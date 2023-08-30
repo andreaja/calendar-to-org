d6e876ea79d4465737838ddf69d4984761f3d46f
Andreas Jacobsen <andreaja@ifi.uio.no>
2023-08-30 21:30:17 +0200

    Create a wrapper script so we can run from launchd

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/opt/homebrew/bin/bash
+
+# Detect the directory of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Path to your virtual environment (assuming it's in the same directory)
+VIRTUALENV_DIR="$DIR/.venv"
+
+# Activate the virtual environment
+source "$VIRTUALENV_DIR/bin/activate"
+
+# Execute the Python script (assuming it's in the same directory as this script)
+python "$DIR/cal-to-org.py" > "${ORG_CALENDAR_FILE}"
+
+# Deactivate the virtual environment (optional, since the script ends anyway)
+deactivate
+
